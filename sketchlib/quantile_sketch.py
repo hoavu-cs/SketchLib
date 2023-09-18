52513f0cf183c5b24d577fa14e21cdae73f37b31
Hoa Vu <hoavu.tr@gmail.com>
2023-09-18 15:11:25 -0700

    Update quantile sketch.

--- sketchlib/quantile_sketch.py
+++ sketchlib/quantile_sketch.py
@@ -3,9 +3,7 @@ from math import log2, ceil, floor
 from copy import deepcopy
 
 class QuantileSketch:
-    """
-    A quantile sketch based on Count-Min and dyadic intervals.
-    """
+    """ A quantile sketch based on Count-Min and dyadic intervals. """
 
     def __init__(self, epsilon=0.1, delta=0.01, max_count=10**9, n=10**9, seed=42):
         """
@@ -24,9 +22,7 @@ class QuantileSketch:
                           for _ in range(self._num_dyadic_intervals + 1)]
 
     def _decompose_into_dyadic_intervals(self, lower, upper):
-        """
-        Compute the dyadic intervals decomposition of [lower, upper].
-        """
+        """ Compute the dyadic intervals decomposition of [lower, upper]. """
         if lower > upper:
             return []
         elif lower == upper:
@@ -35,14 +31,11 @@ class QuantileSketch:
         return [(lower, split_point)] + self._decompose_into_dyadic_intervals(split_point + 1, upper)
 
     def _positions_in_intervals(self, x):
-        """
-        Return the position of the dyadic intervals that x belongs to in each level.
-        """
-        return [ceil(x / (1 << i)) for i in range(self._num_dyadic_intervals + 1)]
+        """ Return the position of the dyadic intervals that x belongs to in each level. """
+        return [ceil(x / (2 ** i)) for i in range(self._num_dyadic_intervals + 1)]
 
     def _estimate_total_count_given_intervals(self, intervals):
-        """
-        Given a list of dyadic intervals, return the estimate of the count of elements 
+        """ Given a list of dyadic intervals, return the estimate of the count of elements 
         in the union of these intervals.
         """
         total_count = 0
@@ -53,17 +46,13 @@ class QuantileSketch:
         return total_count
 
     def insert(self, x, count=1):
-        """
-        Insert an element x into the sketch with a given count.
-        """
+        """ Insert an element x into the sketch with a given count. """
         for i, pos in enumerate(self._positions_in_intervals(x)):
             self._cm_sketch[i].insert(str(pos), count)
         self._l1_norm += count        
 
     def query(self, q):
-        """
-        Query the sketch for the qth quantile.
-        """
+        """ Query the sketch for the qth quantile. """
         threshold, lower, upper = q * self._l1_norm, 1, self._range_elements
         result = None
         while lower <= upper:
@@ -76,25 +65,19 @@ class QuantileSketch:
         return result
 
     def merge(self, other):
-        """
-        Merge self with another compatible sketch (same seed, epsilon, and delta)
-        """
+        """ Merge self with another compatible sketch (same seed, epsilon, and delta) """
         self._l1_norm += other._l1_norm
         for i in range(self._num_dyadic_intervals + 1):
             self._cm_sketch[i].merge(other._cm_sketch[i])
 
     def __add__(self, other):
-        """
-        Return a new sketch that is the merge of self and other.
-        """
+        """ Return a new sketch that is the merge of self and other. """
         merged_sketch = deepcopy(self)
         merged_sketch.merge(other)
         return merged_sketch
 
     @classmethod
     def from_existing(cls, original):
-        """
-        Create a new instance from an existing instance.
-        """
+        """ Create a new instance from an existing instance. """
         return cls(epsilon=original._epsilon, delta=original._delta, max_count=original._max_count,
                    n=original._range_elements, seed=original._seed)
