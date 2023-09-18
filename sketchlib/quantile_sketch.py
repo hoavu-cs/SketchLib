7bfc728aa2077ff952141afa65873ad4033b8338
Hoa Vu <hoavu.tr@gmail.com>
2023-09-18 13:55:33 -0700

    update quantile sketch

--- sketchlib/quantile_sketch.py
+++ sketchlib/quantile_sketch.py
@@ -1,139 +1,97 @@
-# In progress
 from sketchlib.count_min import CountMin
 from math import log2, ceil, floor
+from copy import deepcopy
 
 class QuantileSketch:
     """
-    A quantile sketch based on Count-Min.
+    A quantile sketch based on Count-Min and dyadic intervals.
     """
 
-    def __init__(self, epsilon=0.1, delta=0.01, m=10**9, n=10**9, seed=42):
+    def __init__(self, epsilon=0.1, delta=0.01, max_count=10**9, n=10**9, seed=42):
         """
         epsilon: error bound
         delta: probability of error
         m: maximum number of elements in the stream
         n: the elements in the stream are in the range [1, n]
         """
-        self.epsilon = epsilon
-        self.delta = delta
-        self.max_count = m
-        self.range_elements = n
-        self.l1_norm = 0
-        self.num_dyadic_intervals = ceil(log2(n))
-
+        self._epsilon, self._delta, self._max_count, self._range_elements = epsilon, delta, max_count, n
+        self._l1_norm, self._seed = 0, seed
+        self._num_dyadic_intervals = ceil(log2(n))
+        
         # Initialize Count-Min sketch for each dyadic interval
-        cm_sketch_width = ceil(4*log2(self.max_count)/self.epsilon)   
-        self.cm_sketch = [
-            CountMin(width=cm_sketch_width, delta=self.delta, seed=seed)
-            for _ in range(self.num_dyadic_intervals + 1)
-        ]
+        cm_sketch_width = int(4 * log2(self._max_count) / epsilon)
+        self._cm_sketch = [CountMin(width=cm_sketch_width, delta=delta, seed=seed)
+                          for _ in range(self._num_dyadic_intervals + 1)]
 
     def _decompose_into_dyadic_intervals(self, lower, upper):
         """
         Compute the dyadic intervals decomposition of [lower, upper].
         """
-        if lower < 1:
-            raise ValueError("Lower bound must be at least 1.")
-        
-        if lower == upper:
-            return [(lower, upper)]
-        
         if lower > upper:
             return []
-
+        elif lower == upper:
+            return [(lower, upper)]
+            
         split_point = lower + 2 ** floor(log2(upper - lower)) - 1
-        intervals = [(lower, split_point)]
-        intervals += self._decompose_into_dyadic_intervals(split_point + 1, upper)
-        return intervals
+        return [(lower, split_point)] + self._decompose_into_dyadic_intervals(split_point + 1, upper)
 
     def _positions_in_intervals(self, x):
         """
         Return the position of the dyadic intervals that x belongs to in each level.
-        For example, if n = 20, then we have the following dyadic intervals:
-        level 2^0: [1, 1], [2, 2], ..., [19, 19], [20, 20]
-        level 2^1: [1, 2], [3, 4], ..., [19, 20]
-        level 2^2: [1, 4], [5, 8], ..., [17, 20]
-        ...
-        For each level, return the position of x in the dyadic interval.
-        For example, if x = 5, then return [5, 3, 2, 1, 1] because x is in the dyadic intervals
-        [5, 5], [5, 6], [5, 8], [1, 16], [1, 32].
         """
-        positions = []
-        for i in range(self.num_dyadic_intervals + 1):
-            length = 2 ** i
-            position = ceil(x / length)
-            positions.append(position)
-        return positions
+        return [ceil(x / (1 << i)) for i in range(self._num_dyadic_intervals + 1)]
 
     def _estimate_total_count_given_intervals(self, intervals):
         """
         Given a list of dyadic intervals, return the estimate of the count of elements 
         in the union of these intervals.
         """
-        total_count = 0
-        for a, b in intervals:
-            level = int(log2(b - a + 1))
-            position = ceil(a / 2 ** level)
-            total_count += self.cm_sketch[level].estimate_count(str(position))
-        return total_count
+        return sum(self._cm_sketch[int(log2(b - a + 1))].estimate_count(str(ceil(a / (1 << int(log2(b - a + 1))))))
+                  for a, b in intervals)
 
     def insert(self, x, count=1):
         """
         Insert an element x into the sketch with a given count.
         """
-        positions = self._positions_in_intervals(x)
-        for i, pos in enumerate(positions):
-            self.cm_sketch[i].insert(str(pos), count)
-        self.l1_norm += count        
+        for i, pos in enumerate(self._positions_in_intervals(x)):
+            self._cm_sketch[i].insert(str(pos), count)
+        self._l1_norm += count        
 
     def query(self, q):
         """
-        Query the sketch for the qth quantile. Return the smallest i such that 
-        count(i) >= q * l1_norm and count(i - 1) < (q + epsilon) * l1_norm.
+        Query the sketch for the qth quantile.
         """
-        threshold = q * self.max_count
-        lower, upper = 1, self.max_count
-
-        while lower < upper:
+        threshold, lower, upper = q * self._l1_norm, 1, self._max_count
+        result = None
+        while lower <= upper:
             mid = (lower + upper) // 2
-            intervals = self._decompose_into_dyadic_intervals(1, mid)
-            total_count = self._estimate_total_count_given_intervals(intervals)
-
-            if total_count > q * self.l1_norm:
-                upper = mid
-            elif total_count < q * self.l1_norm:
+            total_count = self._estimate_total_count_given_intervals(self._decompose_into_dyadic_intervals(1, mid))
+            if total_count < threshold:
                 lower = mid + 1
             else:
-                return mid
+                result, upper = mid, mid - 1
+        return result
 
-        intervals = self._decompose_into_dyadic_intervals(1, lower)
-        total_count = self._estimate_total_count_given_intervals(intervals)
-
-        if total_count > q * self.l1_norm:
-            return lower
-        else:
-            return lower + 1
-
-    def merge(self, other_sketch):
-        """  Merge self with another compatible sketch (same seed, epsilon, and delta)
-        """ 
-        self.l1_norm += other_sketch.l1_norm
-        for i in range(self.num_dyadic_intervals + 1):
-            self.cm_sketch[i].merge(other_sketch.cm_sketch[i])
+    def merge(self, other):
+        """
+        Merge self with another compatible sketch (same seed, epsilon, and delta)
+        """
+        self._l1_norm += other._l1_norm
+        for i in range(self._num_dyadic_intervals + 1):
+            self._cm_sketch[i].merge(other._cm_sketch[i])
 
     def __add__(self, other):
-        """  Return a new sketch that is the merge of self and other.
-        """ 
+        """
+        Return a new sketch that is the merge of self and other.
+        """
         merged_sketch = deepcopy(self)
         merged_sketch.merge(other)
         return merged_sketch
 
     @classmethod
     def from_existing(cls, original):
-        new_instance = cls(epsilon=original.epsilon, delta=original.delta, m=original.max_count,\
-            n=original.range_elements, seed=original.seed)
-        return new_instance
-
-
-        
-
+        """
+        Create a new instance from an existing instance.
+        """
+        return cls(epsilon=original._epsilon, delta=original._delta, max_count=original._max_count,
+                   n=original._range_elements, seed=original._seed)
